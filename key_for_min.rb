fa983daa68b03abf586266c807b4714bae8eaf48
Vanna Winland <vgwinland@gmail.com>
2019-09-18 22:19:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
   key_for_min_value do |key, value|
     if min_value == nil || value < min_value
       min_value = value
-      
+      min_key = key
+    end
     
 end
