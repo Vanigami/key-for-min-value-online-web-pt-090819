85c11a1f88792e97e7619405d2d7a1dded6343e1
Vanna Winland <vgwinland@gmail.com>
2019-09-18 22:05:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-   do |name, value|
+  key_for_min_value do |name, value|
     value
     if value == [-1]
       puts name
